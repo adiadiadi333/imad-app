77d6baf59b5c200a11aad31af5872829a43213c2
adiadiadi333 <30661930+adiadiadi333@users.noreply.github.com>
2017-08-04 22:11:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/article2', function (req, res) {
 });
 
 app.get('/article3', function (req, res) {
-  res.send('You asked article three. Here it is');
+  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
 app.get('/ui/madi.png', function (req, res) {

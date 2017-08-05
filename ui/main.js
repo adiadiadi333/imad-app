5de99e1e46f576faf192629b3198c005a2672e54
adiadiadi333 <30661930+adiadiadi333@users.noreply.github.com>
2017-08-05 19:38:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 
 // image = the image of the dinosour
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 // if i click do
 img.onclick = function(){
     img.style.marginLeft = '100px'; 
